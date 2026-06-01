02492f69836e90fb6e07040f8120719d88518a38
SlytherinKid <mr.shubhamkr2000@gmail.com>
2026-06-01 22:57:20 +0530

    added the feature of extracting audio from video

---
+++ utils/audio_processor.py
@@ -0,0 +1,40 @@
+import yt_dlp
+from pydub import AudioSegment
+import os
+
+DOWNLOAD_DIR = 'downloads'
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+FFMPEG_PATH = r"C:\ffmpeg\ffmpeg\bin\ffmpeg.exe"
+
+def download_youtube_audio(url: str) -> str:
+    output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": output_path,
+        "ffmpeg_location": FFMPEG_PATH,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "wav",
+                "preferredquality": "192",
+            }
+        ],
+        "quiet": True,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
+    return filename
+
+data = download_youtube_audio("https://youtu.be/eVli-tstM5E?si=ZLBbHJmbXkGNcEIF")
+
+def convert_to_wav(input_path: str) -> str:
+    """Convert any audio/video file to WAV format using pydub."""
+    output_path = os.path.splitext(input_path)[0] + "_converted.wav"
+    audio = AudioSegment.from_file(input_path)
+    audio = audio.set_channels(1).set_frame_rate(16000) #16khz
+    audio.export(output_path, format="wav")
+    return output_path
+
+print(convert_to_wav(data))
